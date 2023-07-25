35465ec20847ef3930b029f57ceaaa805d2bff25
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-07-25 18:47:36 -0400

    Complete Lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i-=1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num**2 for num in int_list]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if(i%3==0 and i%5==0):
+            print("FizzBuzz")
+            i+=1
+            continue
+        elif(i%3==0):
+            print("Fizz")
+            i+=1
+            continue
+        elif(i%5==0):
+            print("Buzz")
+            i+=1
+            continue
+        else:
+            print(i)
+            i+=1
